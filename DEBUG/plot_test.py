ee24e0e4ae2dd23606eeddba38b4de1462033ea7
wty-yy <wty-yy@github.com>
2023-08-23 10:16:57 +0800

    remove debug

--- DEBUG/plot_test.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-# a = np.loadtxt(r"../wrong_action.txt").astype("uint32")
-a = np.load(r"../logs/(8,210,160,3)_state.txt.npy")
-print(a.shape)
-plt.imshow(a[0])
-plt.show()
